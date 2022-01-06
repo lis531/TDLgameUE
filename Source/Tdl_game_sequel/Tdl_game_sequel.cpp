e47ab4d269ed9e2e238f884551c7bb88b2ff8668
lis531 <lisekminecraftowy175@gmail.com>
2022-01-06 19:16:33 +0100

    .

--- Source/Tdl_game_sequel/Tdl_game_sequel.cpp
+++
@@ -1,7 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "Tdl_game_sequel.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Tdl_game_sequel, "Tdl_game_sequel" );
- 
