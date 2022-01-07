4cbef2df59667fdf3979b40305c96270e2503ccb
lis531 <lisekminecraftowy175@gmail.com>
2022-01-07 12:43:34 +0100

    .

---
+++ Source/Tdl_game_sequel/Tdl_game_sequelGameMode.cpp
@@ -0,0 +1,13 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Tdl_game_sequelGameMode.h"
+#include "Tdl_game_sequelCharacter.h"
+#include "UObject/ConstructorHelpers.h"
+
+ATdl_game_sequelGameMode::ATdl_game_sequelGameMode()
+	: Super()
+{
+	// set default pawn class to our Blueprinted character
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/FirstPersonCPP/Blueprints/FirstPersonCharacter"));
+	DefaultPawnClass = PlayerPawnClassFinder.Class;
+}
---
+++ Source/Tdl_game_sequel/Tdl_game_sequelGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "Tdl_game_sequelGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ATdl_game_sequelGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ATdl_game_sequelGameMode();
+};
+
+
+
